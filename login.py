dcd291cb4e4cb73fc0439d515c02e6df5fbd29f1
adamelsayed06 <adame68@nycstudents.net>
2025-03-19 23:59:43 -0400

    finish login

--- login.py
+++ login.py
@@ -23,4 +23,9 @@ driver.get("https://www.linkedin.com/login")
 time.sleep(5) # wait for page to load 5s
 
 username = driver.find_element(By.ID, "username") # <input id="username"> on linkedin website, searches for this tag
-username.send_keys("your_username_here") # enters username
+username.send_keys(USERNAME) # enters username
+
+password = driver.find_element(By.ID, "password") # <input id="password"> on linkedin website, searches for this tag
+password.send_keys(PASSWORD) # enters password
+
+driver.find_element(By.XPATH, "//button[@type='submit']").click() #find button with type submit (login button) and clicks it
