49a5fcfd7bdf25a22e95e2a82f1f938ae86f9dac
adamelsayed06 <adame68@nycstudents.net>
2025-03-20 00:23:35 -0400

    refactor into functions and scroll function

--- login.py
+++ login.py
@@ -3,7 +3,6 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from bs4 import BeautifulSoup
 from dotenv import load_dotenv
 import os
 import time
@@ -18,14 +17,25 @@ PASSWORD = os.getenv("PASSWORD")
 service = Service(ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service)
 
-driver.get("https://www.linkedin.com/login")
+def login():
+    driver.get("https://www.linkedin.com/login")
 
-time.sleep(5) # wait for page to load 5s
+    time.sleep(5) # wait for page to load 5s
 
-username = driver.find_element(By.ID, "username") # <input id="username"> on linkedin website, searches for this tag
-username.send_keys(USERNAME) # enters username
+    username = driver.find_element(By.ID, "username") # <input id="username"> on linkedin website, searches for this tag
+    username.send_keys(USERNAME) # enters username
 
-password = driver.find_element(By.ID, "password") # <input id="password"> on linkedin website, searches for this tag
-password.send_keys(PASSWORD) # enters password
+    password = driver.find_element(By.ID, "password") # <input id="password"> on linkedin website, searches for this tag
+    password.send_keys(PASSWORD) # enters password
 
-driver.find_element(By.XPATH, "//button[@type='submit']").click() #find button with type submit (login button) and clicks it
+    driver.find_element(By.XPATH, "//button[@type='submit']").click() #find button with type submit (login button) and clicks it
+
+def open_profile_and_scroll(profile_url):
+    driver.get(profile_url) # opens your profile
+    time.sleep(5) # wait for page to load 5s
+    
+    #scrolls to the bottom of the page
+    start = time.time()
+    while time.time() - start < 5: #scrolls for 5 seconds
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    
---
+++ scraper.py
@@ -0,0 +1,4 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from bs4 import BeautifulSoup
+import time
