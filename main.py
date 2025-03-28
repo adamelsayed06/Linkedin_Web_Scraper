8180ab3411f204ea6adf0f94a42457ab200268f7
adamelsayed06 <adamelsayyed06@gmail.com>
2025-03-27 22:38:34 -0400

    DB VARIABLES

--- main.py
+++ main.py
@@ -14,6 +14,11 @@ import psycopg2
 
 load_dotenv()
 
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
 USERNAME = os.getenv("LINKEDIN_USERNAME")
 PASSWORD = os.getenv("LINKEDIN_PASSWORD")
 ACCESSIBILITY_KEYWORDS = {
